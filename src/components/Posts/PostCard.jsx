a207c238777042b58d8e2c110ad22046fe4db757
3MooRa-0 <amr054327@gmail.com>
2024-01-26 07:42:00 +0200

    imporving animation

--- src/components/Posts/PostCard.jsx
+++ src/components/Posts/PostCard.jsx
@@ -4,26 +4,28 @@ import { Link } from "react-router-dom";
 
 const PostCard = ({ _id, title, description, imageUrl, author, createdAt }) => {
   return (
-    <div className="bg-primary max-w-3xl rounded-xl overflow-hidden shadow-lg flex gap-5 mt-8 max-sm:flex-col max-sm:items-center min-w-full border-[1px] border-[var(--primary-color)] p-4">
-      <Link to={`/post/${_id}`}>
-        <div className=" flex items-center justify-center w-60 h-60 hover:scale-110 transition duration-300">
-          <img
-            className=" max-w-60 max-h-60 rounded-md"
-            src={`${import.meta.env.VITE_HOSTING_URL}/${imageUrl}`}
-            alt="Post Image"
-          />
-        </div>
-      </Link>
-      <div className="sm:px-6 max-sm:py-4 cursor-pointer hover:scale-105 transition duration-300">
+    <div className="bg-primary max-w-3xl rounded-xl overflow-hidden shadow-lg  mt-8   min-w-full border-[1px] border-[var(--primary-color)] p-4  hover:scale-105 transition duration-1000">
+      <div className="flex gap-5 max-md:flex-col max-md:items-center">
         <Link to={`/post/${_id}`}>
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <div className="flex gap-3 mb-2 text-sm text-gray-500">
-            <p>{author.name} |</p>
-            <p>{format(new Date(createdAt), "MMM d, yyyy")}</p>
+          <div className=" flex items-center justify-center w-60 h-60 ">
+            <img
+              className=" max-w-60 max-h-60 rounded-md"
+              src={`${import.meta.env.VITE_HOSTING_URL}/${imageUrl}`}
+              alt="Post Image"
+            />
           </div>
-
-          <p className="text-gray-300 text-base">{description}</p>
         </Link>
+        <div className="sm:px-6 max-sm:py-4 cursor-pointer">
+          <Link to={`/post/${_id}`}>
+            <div className="font-bold text-xl mb-2">{title}</div>
+            <div className="flex gap-3 mb-2 text-sm text-gray-500">
+              <p>{author.name} |</p>
+              <p>{format(new Date(createdAt), "MMM d, yyyy")}</p>
+            </div>
+
+            <p className="text-gray-300 text-base">{description}</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
